11a36113d52a4fa8ac608880b1317d9c9c51b50e
frank_chen <792151095@qq.com>
2018-06-14 17:20:23 +0800

    初始化项目

--- Classes/views/NumberKeypadView.m
+++ Classes/views/NumberKeypadView.m
@@ -5,6 +5,10 @@
 //  Created by chenyuan on 2018/5/17.
 //
 
+#define isIphone5 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size) : NO)
+#define isIphone6Plus ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? (CGSizeEqualToSize(CGSizeMake(1125, 2001), [[UIScreen mainScreen] currentMode].size) || CGSizeEqualToSize(CGSizeMake(1242, 2208), [[UIScreen mainScreen] currentMode].size)) : NO)
+#define isIphoneX ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) : NO)
+
 #import "NumberKeypadView.h"
 
 @implementation NumberKeypadView{
